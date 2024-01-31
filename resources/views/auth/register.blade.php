13fd49879a1af5eed8a89c60749d12dee70c6143
Aqila <sitiainuraqila@gmail.com>
2024-01-31 15:41:12 +0800

    git Procfile

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -89,14 +89,5 @@
             </div>
         </div>
     </div>
+    
 @endsection
-
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
